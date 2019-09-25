38368b8bab03ca1bbe17222c2f7c31991bcff6e9
Airton Toyansk <atoyansk@gmail.com>
2019-09-26 01:33:46 +0300

    Animated tabs - first version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+  tab1 = 'DatePage';
+  tab2 = 'GradesPage';
+  tab3 = 'TasksPage';
+  tab4 = 'MessagesPage';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
   }
 
 }
