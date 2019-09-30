515419c8e16d533c084325d03b1dc2a7c98467ac
Airton Toyansk <atoyansk@gmail.com>
2019-10-01 02:28:06 +0300

    Transitions and Design details

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
 
 @IonicPage()
 @Component({
@@ -12,11 +13,48 @@ export class HomePage {
   tab3 = 'TasksPage';
   tab4 = 'MessagesPage';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loaded:   boolean = false;
+  tabIndex: number  = 0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private nativePageTransitions: NativePageTransitions) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
+  }
+
+  private getAnimationDirection(index:number):string {
+    var currentIndex = this.tabIndex;
+
+    this.tabIndex = index;
+
+    switch (true){
+      case (currentIndex < index):
+        return('left');
+      case (currentIndex > index):
+        return('right');
+    }
+  }
+
+  public transition(e:any):void {    
+    let options: NativeTransitionOptions = {
+     direction:this.getAnimationDirection(e.index),
+     duration: 250,
+     slowdownfactor: -1,
+     slidePixels: 0,
+     iosdelay: 20,
+     androiddelay: 0,
+     fixedPixelsTop: 0,
+     fixedPixelsBottom: 48
+    };
+
+    if (!this.loaded) {
+      this.loaded = true;
+      return;
+    }
+
+    this.nativePageTransitions.fade(null);
   }
 
 }
