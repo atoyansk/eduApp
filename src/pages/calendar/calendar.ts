0dd77cf74bd509373987c60b30524723503487e9
Airton Toyansk <atoyansk@gmail.com>
2019-10-15 01:52:02 +0300

    Calendar Page - basic functions

--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -8,13 +8,72 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CalendarPage {
 
-  somethings: any = new Array(20);
+  date: any;
+  daysInThisMonth: any;
+  daysInLastMonth: any;
+  daysInNextMonth: any;
+  monthNames: string[];
+  currentMonth: any;
+  currentYear: any;
+  currentDate: any;
+
+  showSpinner: boolean = true;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.date = new Date();
+    this.monthNames = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    this.getDaysOfMonth();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CalendarPage');
+  ionViewDidLoad() {}
+
+  getDaysOfMonth() {
+    this.daysInThisMonth = new Array();
+    this.daysInLastMonth = new Array();
+    this.daysInNextMonth = new Array();
+    this.currentMonth = this.monthNames[this.date.getMonth()];
+    this.currentYear = this.date.getFullYear();
+    if(this.date.getMonth() === new Date().getMonth()) {
+      this.currentDate = new Date().getDate();
+    } else {
+      this.currentDate = 999;
+    }
+
+    //this.showSpinner = false;
+  
+    var firstDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth(), 1).getDay();
+    var prevNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth(), 0).getDate();
+    for(var i = prevNumOfDays-(firstDayThisMonth-1); i <= prevNumOfDays; i++) {
+      this.daysInLastMonth.push(i);
+    }
+  
+    var thisNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDate();
+    for (var j = 0; j < thisNumOfDays; j++) {
+      this.daysInThisMonth.push(j+1);
+    }
+  
+    var lastDayThisMonth = new Date(this.date.getFullYear(), this.date.getMonth()+1, 0).getDay();
+    var nextNumOfDays = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0).getDate();
+    for (var k = 0; k < (6-lastDayThisMonth); k++) {
+      this.daysInNextMonth.push(k+1);
+    }
+    var totalDays = this.daysInLastMonth.length+this.daysInThisMonth.length+this.daysInNextMonth.length;
+    if(totalDays<36) {
+      for(var p = (7-lastDayThisMonth); p < ((7-lastDayThisMonth)+7); p++) {
+        this.daysInNextMonth.push(p);
+      }
+    }
+  }
+
+  goToLastMonth() {
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth(), 0);
+    this.getDaysOfMonth();
+  }
+
+  goToNextMonth() {
+    this.date = new Date(this.date.getFullYear(), this.date.getMonth()+2, 0);
+    this.getDaysOfMonth();
   }
 
 }
